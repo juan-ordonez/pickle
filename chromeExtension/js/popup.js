a566816eda72ae5269f755f724621cca79b8f514
Juan Ordonez <juan@gopikolo.com>
2017-10-23 19:37:12 -0700

    Added auto-scroll to bottom on popup.html

--- chromeExtension/js/popup.js
+++ chromeExtension/js/popup.js
@@ -1,2 +1,13 @@
+//Scroll to bottom of chat page each time popup is opened
+$(".containerComments").scrollTop($(".containerComments")[0].scrollHeight);
+
 //Autosize textarea for new comments
 autosize(document.querySelectorAll('#newComment'));
+//Scroll to bottom of page after each textarea resize
+$('#newComment').each(function(){
+  autosize(this);
+}).on('autosize:resized', function(){
+  $(".containerComments").scrollTop($(".containerComments")[0].scrollHeight);
+});
+
+

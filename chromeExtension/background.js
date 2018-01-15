a424ffbbbe8d423da0b82826ed4124359db4c64a
Juan Ordonez <juan@gopikolo.com>
2018-01-14 17:33:12 -0800

    fixed user getting notification when posting to general

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -217,7 +217,7 @@ chrome.gcm.onMessage.addListener(function(payload) {
 
   if (type == "notification") {
 
-  if (views.length == 0) {
+  if (userID != payload.data.poster) {
   //console.log("popup is shut");
 
   chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
@@ -782,8 +782,7 @@ function comment(userID, url, value, tags, all, picture, pageTitle, checked, cur
           //If comment is public, then notification should say that user tagged the recipient
           if (checked) {
             var array = data.slice();
-            console.log("PUBLIC");
-            var json = JSON.stringify({"data" : {"status" : "tagged you", "pic" : picture, "first" : userName, "comment" : value, "commentID" : comment, "url" : url, "pageTitle" : pageTitle, "type" : "notification", "groupID" : currentGroup, "currentGroupName": currentGroupName, "pageImage" : store['pageImage']}, "registration_ids": data });
+            var json = JSON.stringify({"data" : {"status" : "tagged you", "pic" : picture, "first" : userName, "comment" : value, "commentID" : comment, "url" : url, "pageTitle" : pageTitle, "type" : "notification", "groupID" : currentGroup, "currentGroupName": currentGroupName, "pageImage" : store['pageImage'], "poster" : userID}, "registration_ids": data });
           
             $.post("http://pickle-server-183401.appspot.com/notification/", {"picture" : picture, "user" : userName.split(" ")[0], "notification" : "tagged you on", "cookies" : tags, "url" : url, "page" : pageTitle}, function(notif) {
               // console.log("notify", data, json);
